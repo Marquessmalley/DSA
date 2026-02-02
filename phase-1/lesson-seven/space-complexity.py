2b953cbfd1ac3612b6d1919f553b4050b619cfbd
Marques Smalley <marquessmalley@gmail.com>
2026-02-02 18:23:47 -0500

    Add space complexity explanations to space-complexity.py; include definitions and complexity analysis for clarity on memory usage in algorithms.

--- phase-1/lesson-seven/space-complexity.py
+++ phase-1/lesson-seven/space-complexity.py
@@ -1,3 +1,8 @@
+# Space Complexity is the amount of memory used by an algorithm.
+# It is measured in units of memory.
+
+
+# O(n) space complexity & Time Complexity O(n) becasue we are using a set to store the numbers and then comparing the length of the set to the length of the list.
 def contains_duplicate(nums):
   return len(set(nums)) != len(nums)
 
